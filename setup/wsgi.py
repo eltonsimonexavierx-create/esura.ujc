40363312008e9120d78f40f9d29236ad3a062070
Elton <eltonsimonexavierx@gmail.com>
2026-04-24 08:24:44 +0200

    Fix 404 path

--- setup/wsgi.py
+++ setup/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for setup project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
+# 1. Substitua 'NOME_DO_SEU_PROJETO' pelo nome da pasta onde está o seu settings.py
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NOME_DO_SEU_PROJETO.settings')
 
 application = get_wsgi_application()
+
+# 2. Esta linha é obrigatória para a Vercel encontrar o projeto
+app = application
